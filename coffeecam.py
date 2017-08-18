c73e84f103d265101ee9cf6bf38c0a988183b8e7
Jason R. Jones <jjones@updsys.com>
2017-08-17 21:48:45 -0400

    Added some neat stats

--- coffeecam.py
+++ coffeecam.py
@@ -6,6 +6,7 @@ import datetime
 import flask
 import picamera
 from waitress import serve
+import humanize
 
 
 logger = logging.getLogger(__name__)
@@ -14,18 +15,56 @@ logging.basicConfig(level=logging.DEBUG)
 cam = picamera.PiCamera()
 last_pic_time = datetime.datetime.now()
 
+users = set()
+user_logins = dict()
+havent_logged_in_since = dict()
+
+logged_in = dict()
+
 app = flask.Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return flask.render_template('index.jinja2')
+    user_id = flask.request.remote_addr.split('.')[-1]
+    users.add(user_id)
+
+    if user_id not in user_logins.keys():
+        user_logins[user_id] = 1
+    else:
+        user_logins[user_id] += 1
+
+    last_logged_in = havent_logged_in_since.get(user_id)
+    if last_logged_in is not None:
+        td = datetime.datetime.now() - last_logged_in
+        last_logged_in = humanize.naturaldelta(td)
+
+    else:
+        last_logged_in = 'never'
+
+    havent_logged_in_since[user_id] = datetime.datetime.now()
+
+    return flask.render_template(
+        'index.jinja2',
+        users=len(users),
+        your_logins=user_logins[user_id],
+        user_logins=find_most_logins(user_logins),
+        user_id=user_id,
+        last_login=last_logged_in
+
+    )
 
 
 @app.route('/about')
 def about():
     return flask.render_template('about.jinja2')
+
+
+@app.route('/set_time')
+def set_time():
+    print('not implemented')
+    return '', 204
 
 
 @app.route('/take_pic', methods=['POST'])
@@ -73,6 +112,23 @@ def md5(fname):
         return hash_md5.hexdigest()
     except FileNotFoundError:
         return '0'
+
+
+def find_most_logins(user_logins):
+    u = list(user_logins.keys())[0]
+
+    if len(u) == 0:
+        return '-', '-'
+
+    l = user_logins.get(u)
+
+    for user, logins in user_logins.items():
+        if logins > l:
+            u = user
+            l = logins
+
+    return u, l
+
 
 serve(app, host='0.0.0.0', port=80)
 #app.run(host='0.0.0.0', port=80, debug=True)
