b37b6bf6ff29dfb9ca60c3d6f10bf36d56e7c59f
Jason R. Jones <jjones@updsys.com>
2017-08-20 23:45:34 -0400

    Added configuration file parsing

--- coffeecam/config.py
+++ coffeecam/config.py
@@ -1,5 +1,44 @@
-MEDIA_URL = '/ram/'
-MEDIA_DIR = '/home/jason/ram/'
-MEDIA_NAME = 'live.jpg'
+"""
+Parses the supplied configuration file
+"""
 
-show_stats = True
+import sys
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+if '-c' not in sys.argv and '--config' not in sys.argv:
+    logger.error('!!! configuration not found !!!')
+    sys.exit(1)
+
+if '-c' in sys.argv:
+    index_of_config = sys.argv.index('-c') + 1
+elif '--config' in sys.argv:
+    index_of_config = sys.argv.index('--config') + 1
+
+logging.debug('config index: {} ({})'.format(index_of_config, sys.argv[index_of_config]))
+
+with open(sys.argv[index_of_config], 'r') as f:
+    config = json.loads(f.read())
+
+MEDIA_URL = config.get('web').get('media url')
+
+if not MEDIA_URL.startswith('/'):
+    MEDIA_URL = '/' + MEDIA_URL
+if not MEDIA_URL.endswith('/'):
+    MEDIA_URL = MEDIA_URL + '/'
+
+MEDIA_DIR = config.get('local').get('directory')
+
+if not MEDIA_DIR.startswith('/'):
+    MEDIA_DIR = '/' + MEDIA_DIR
+if not MEDIA_DIR.endswith('/'):
+    MEDIA_DIR = MEDIA_DIR + '/'
+
+MEDIA_NAME = config.get('local').get('name')
+
+show_stats = config.get('web').get('stats')
+show_stats = True if show_stats else False
