18b6e680cf0795d2c8bd4564e4364fb5ea3520fb
Jason R. Jones <jjones@updsys.com>
2017-08-27 19:33:33 -0400

    Refactoring to something more similar to a video feed

--- coffeecam/static/js/custom.js
+++ coffeecam/static/js/custom.js
@@ -1,20 +1,13 @@
 $(document).ready(
     function(){
-        setInterval(function(){
-            $.post("/take_pic", {}, function(data){replacePic(data);});
-        },
-        10000);
-
         /* submit time using a post request */
         var date = new Date();
-        $.post('/set_time', {"date": date.getTime(), 'timezoneOffset': date.getTimezoneOffset() * 60.0});
+        $.post(
+            '/set_time',
+            {
+                "date": date.getTime(),
+                'timezoneOffset': date.getTimezoneOffset() * 60.0
+            }
+        );
     }
 );
-
-function replacePic(data){
-    var pic = $('#coffeepic');
-
-    pic.fadeOut(200);
-    pic.attr("src", data['src']);
-    pic.fadeIn(200);
-}
