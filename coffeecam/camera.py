d0f5f343b5067d9c54c5ed6f76a99c4689d40387
Jason R. Jones <jjones@updsys.com>
2017-08-28 20:46:33 -0400

    Refactored camera based on blog post

--- coffeecam/camera.py
+++ coffeecam/camera.py
@@ -1,46 +1,28 @@
-from datetime import datetime, timedelta
-from io import BytesIO
-import picamera
-from time import time
+import time
 import os
+import logging
+
+from coffeecam.base_camera import BaseCamera
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
-class Camera:
-    def __init__(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        dir_path = str(dir_path) + '/static/img/'
-        self.frames = [open(dir_path + f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+class Camera(BaseCamera):
+    """An emulated camera implementation that streams a repeated sequence of
+    files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
 
-    def get_frame(self):
-        return self.frames[int(time()) % 3]
+    # img_path = str(os.path.realpath(__file__)) + '/static/img/'
+    img_path = os.path.dirname(__file__) + '/static/img/'
+    paths = []
 
+    for i in range(10):
+        paths.append(img_path + str(i) + '.jpg')
 
-'''
-class Camera:
-    def __init__(self, framerate=1.0, test=False):
-        if framerate > 0.0:
-            self.interval = 1/framerate
-        else:
-            self.interval = 0
+    images = [open(path, 'rb').read() for path in paths]
 
-        self.test = test
-
-        if not self.test:
-            self.camera = picamera.PiCamera()
-
-        self.last_snapshot = datetime.now()
-        self.stream = BytesIO()
-
-    def snapshot(self):
-        if self.last_snapshot + timedelta(seconds=self.interval) > datetime.now():
-            self.stream.truncate()
-            self.camera.capture(self.stream, 'jpeg')
-            self.last_snapshot = datetime.now()
-
-    def get_frame(self):
-        self.stream.seek(0)
-        yield self.stream.read()
-        '''
-
-
-
+    @staticmethod
+    def frames():
+        while True:
+            time.sleep(0.5)
+            yield Camera.images[int(time.time()) % 10]
